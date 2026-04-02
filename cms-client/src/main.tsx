8a59898b9c9692268a98b2bd0fc7d63b1384b2e1
Ashutoshbind15 <91549538+Ashutoshbind15@users.noreply.github.com>
2026-04-02 17:58:15 +0000

    add shadcn-ui

--- cms-client/src/main.tsx
+++ cms-client/src/main.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
+import { Button } from "./components/ui/button";
 
 const App = () => {
   return (
     <div className="text-3xl font-bold underline">
       <p>CMS Client</p>
+      <Button
+        onClick={() => {
+          console.log("Button clicked");
+        }}
+      >
+        Click me
+      </Button>
     </div>
   );
 };
