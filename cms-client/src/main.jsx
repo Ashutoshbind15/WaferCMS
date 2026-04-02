3c8497c21692c97d89b476bec97f6e87b3068459
Ashutoshbind15 <91549538+Ashutoshbind15@users.noreply.github.com>
2026-04-02 09:37:31 +0000

    init cms server and client, and the main portfolio app with pnpm config

---
+++ cms-client/src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+
+const App = () => {
+  return (
+    <div>
+      CMS Client
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
