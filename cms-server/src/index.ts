02c2df0ab83c0fd239f4a5cca82c827c4bee2f14
Ashutoshbind15 <91549538+Ashutoshbind15@users.noreply.github.com>
2026-04-03 15:33:23 +0000

    Update TypeScript configurations, implement new blog API and pages in the portfolio-app

--- cms-server/src/index.ts
+++ cms-server/src/index.ts
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import blogRouter from "./routes/blog.js";
-import filesRouter from "./routes/files.js";
+import blogRouter from "./routes/blog";
+import filesRouter from "./routes/files";
 
 const app = express();
 
